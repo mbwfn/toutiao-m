f2c8d4bed40a4d08dd30fd75c3ace5e406ac9bd0
ljx <1509761467@qq.com>
2022-09-02 13:22:36 +0800

    项目初始化-调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+// index 文件可以被快捷路径所忽略
+// index命名的文件对于功能可读性来说是非常差的
+import router from './router/index.js'
+import store from './store/index.js'
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // 通过在component属性中使用箭头函数的形式加载组件的文件资源,实现按需加载的功能, 只有在使用到该路由时, 才
+  //   // 会进行组件文件的加载, 作用： 能够提高第一次开启页面的加载效率
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
